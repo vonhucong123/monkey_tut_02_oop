208e07734e68867c040424ef01a9c6fa265faaeb
Võ Như Công <82624211+VoNhuCong@users.noreply.github.com>
2021-12-22 17:22:48 +0700

    Update main.cpp

--- sourceCode/main.cpp
+++ sourceCode/main.cpp
@@ -3,17 +3,11 @@
 #include "Coronavirus.h"
 #include "Patient.h"
 #include <iostream>
+
 using namespace std;
-enum class an
-{
-	ga,
-	lon
-};
 int main()
 {
-	cout << "start\n";
 	Patient par;
 	par.takeMedicine();
-	cout << "oke";
 	return 0;
 }
