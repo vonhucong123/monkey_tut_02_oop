1dfa0bcdfe5f6156243d99da523a557dd33e11c8
vonhucong123 <96404068+vonhucong123@users.noreply.github.com>
2021-12-21 21:01:47 -0800

    Update BetaCoronavirus.cpp

--- CoronaVirus/CoronaVirus/BetaCoronavirus.cpp
+++ CoronaVirus/CoronaVirus/BetaCoronavirus.cpp
@@ -1,19 +1,53 @@
-#include "BetaCoronavirus.h"
+﻿#include "BetaCoronavirus.h"
+#include <list>
+using namespace std;
 
-void BetaCoronavirus::doBorn()
+BetaCoronavirus::BetaCoronavirus()
+{
+    doBorn();
+}
+
+BetaCoronavirus::~BetaCoronavirus()
 {
 }
 
-void BetaCoronavirus::doClone()
+void BetaCoronavirus::doBorn()
 {
+    loadADNInformation();
+    m_protein = Protein(rand()%3);
+}
+
+list<Coronavirus*> BetaCoronavirus::doClone()
+{
+    list<Coronavirus*> corList;
+    for (int i = 0; i < 1; i++)
+    {
+        BetaCoronavirus *betaCorvi = new BetaCoronavirus(*this);
+        corList.push_back(betaCorvi);
+    }
+    return corList;
 }
 
 void BetaCoronavirus::doDie()
 {
+    m_resistance = 0;
 }
 
 void BetaCoronavirus::initResistance()
 {
+    // xong
+    if (m_protein == Protein::NS3)
+    {
+        m_resistance = rand() % (10 - 1 + 1) + 1;
+    }
+    else if(m_protein == Protein::E)
+    {
+        m_resistance = rand() % (30 - 21 + 1) + 21;
+    }
+    else
+    {
+        m_resistance = rand() % (20 - 11 + 1) + 11;
+    }
 }
 
 int BetaCoronavirus::virusType()
